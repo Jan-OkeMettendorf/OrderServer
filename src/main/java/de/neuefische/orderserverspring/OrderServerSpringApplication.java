b69c02e4fda6a5ab8f32b5836eddf7e6840e305e
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-21 16:16:04 +0200

    Add project

---
+++ src/main/java/de/neuefische/orderserverspring/OrderServerSpringApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/orderserverspring/OrderServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
